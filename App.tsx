c1541c98a096a3fd55344327aa59b33a501d2c63
BartekNowicki <b_nowicki@op.pl>
2023-08-24 14:46:21 +0200

    Add incident saving functionality

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { StatusBar } from "expo-status-bar";
 import * as SQLite from "expo-sqlite";
 
@@ -8,26 +14,15 @@ const db = SQLite.openDatabase("counter.db");
 
 enum Med {
   Ibuprofen = "Ibuprofen",
-  Paracetamol = "Paracetamol"
-}
-
-interface Incident {
-  date: {
-    hour: number;   // assuming 0-23 format
-    day: number;    // assuming 1-31 format
-    month: number;  // assuming 1-12 format
-    year: number;   
-  };
-  med: string;
-  dosage: number;
+  Paracetamol = "Paracetamol",
 }
 
 const App: React.FC = () => {
-  const [date, setDate] = useState<string>('');
-  const [med, setMed] = useState<string>('');
+  const [date, setDate] = useState<string>("");
+  const [med, setMed] = useState<string>("");
   const [dosage, setDosage] = useState<number>(0);
   const [showDatePicker, setShowDatePicker] = useState<boolean>(false);
-  
+
   useEffect(() => {
     console.log("incident db set up initiated");
     db.transaction((tx) => {
@@ -37,31 +32,85 @@ const App: React.FC = () => {
           med text,
           dosage int
       );
-      `);
+      `
+      );
     });
   }, []);
 
   const handleDateChange = (event: any, selectedDate?: Date) => {
     setShowDatePicker(false);
     if (selectedDate) {
-      setDate(`${selectedDate.getDate()}-${selectedDate.getMonth() + 1}-${selectedDate.getFullYear()}`);
+      setDate(
+        `${selectedDate.getDate()}-${
+          selectedDate.getMonth() + 1
+        }-${selectedDate.getFullYear()}`
+      );
+    }
+  };
+
+  const saveIncident = () => {
+    if (date && med && dosage) {
+      db.transaction((tx) => {
+        tx.executeSql(
+          "INSERT INTO incident (date, med, dosage) VALUES (?, ?, ?);",
+          [date, med, dosage],
+          (_, result) => {
+            console.log("Data Inserted Successfully:", result);
+
+            tx.executeSql(
+              "SELECT * FROM incident;",
+              [],
+              (_, resultSet) => {
+                console.log(
+                  "All incidents from the database:",
+                  resultSet.rows._array
+                );
+              },
+              (_, error) => {
+                console.log("Error fetching data:", error);
+                return false;
+              }
+            );
+          },
+          (_, error) => {
+            console.log("Error inserting data:", error);
+            return false;
+          }
+        );
+      });
+    } else {
+      console.log("Please ensure all fields are filled.");
     }
   };
 
   return (
     <View style={styles.container}>
-      {/* Med selection */}
-      <TouchableOpacity style={styles.button} onPress={() => setMed('Ibuprofen')}>
-        <Text style={styles.buttonText}>Ibuprofen</Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          med === Med.Ibuprofen ? styles.selectedButton : {},
+        ]}
+        onPress={() => setMed(Med.Ibuprofen)}
+      >
+        <Text style={styles.buttonText}>{Med.Ibuprofen}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => setMed('Paracetamol')}>
-        <Text style={styles.buttonText}>Paracetamol</Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          med === Med.Paracetamol ? styles.selectedButton : {},
+        ]}
+        onPress={() => setMed(Med.Paracetamol)}
+      >
+        <Text style={styles.buttonText}>{Med.Paracetamol}</Text>
       </TouchableOpacity>
 
-      {/* Date selection */}
-      <TouchableOpacity style={styles.dateButton} onPress={() => setShowDatePicker(true)}>
+      <TouchableOpacity
+        style={[styles.dateButton, date ? styles.selectedButton : {}]}
+        onPress={() => setShowDatePicker(true)}
+      >
         <Text style={styles.buttonText}>Select Date</Text>
       </TouchableOpacity>
+
       {showDatePicker && (
         <DateTimePicker
           value={new Date()}
@@ -71,12 +120,11 @@ const App: React.FC = () => {
         />
       )}
 
-      {/* Dosage input */}
       <TextInput
         style={styles.dosageInput}
         keyboardType="numeric"
         value={String(dosage)}
-        onChangeText={text => {
+        onChangeText={(text) => {
           const num = parseInt(text, 10);
           if (num >= 0 && num <= 1200) {
             setDosage(num);
@@ -88,44 +136,67 @@ const App: React.FC = () => {
       <Text>Date: {date}</Text>
       <Text>Med: {med}</Text>
       <Text>Dosage: {dosage}</Text>
+
+      <TouchableOpacity style={styles.saveButton} onPress={saveIncident}>
+        <Text style={styles.saveButtonText}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   button: {
-    backgroundColor: '#4CAF50',
+    backgroundColor: "#4CAF50",
     padding: 15,
     margin: 10,
     width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
   },
   dateButton: {
-    backgroundColor: '#4CAF50',
-    padding: 10,
+    backgroundColor: "#4CAF50",
+    padding: 15,
     margin: 10,
     width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
   },
   dosageInput: {
     borderWidth: 1,
-    borderColor: '#4CAF50',
+    borderColor: "#4CAF50",
     padding: 10,
     width: 200,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
+  },
+  selectedButton: {
+    backgroundColor: "#2E7D32",
+  },
+  saveButton: {
+    backgroundColor: "#FF0000",
+    padding: 20,
+    margin: 10,
+    marginBottom: 50,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  saveButtonText: {
+    color: "white",
+    fontSize: 20,
   },
 });
 
