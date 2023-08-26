d1c22329f54c2f1d28c7988eb5864be6d61d6e6f
BartekNowicki <b_nowicki@op.pl>
2023-08-26 16:16:18 +0200

    Add graph and db data download, blank buttons for download and share

--- Graph.tsx
+++ Graph.tsx
@@ -1,69 +1,115 @@
-import React, { useCallback, useState } from "react";
-import { View, Button } from "react-native";
-import * as FileSystem from "expo-file-system";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import { BarChart } from "react-native-chart-kit";
 import * as SQLite from "expo-sqlite";
-import * as Sharing from "expo-sharing";
 
 const db = SQLite.openDatabase("incident.db");
 
 const Graph: React.FC = () => {
-  const [fileUri, setFileUri] = useState<string | null>(null);
+  const [data, setData] = useState(null);
 
-  const downloadData = useCallback(async () => {
-    let fetchedData = [];
-
+  useEffect(() => {
     db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT * FROM incident;",
-        [],
-        (_, resultSet) => {
-          for (let i = 0; i < resultSet.rows.length; i++) {
-            fetchedData.push(resultSet.rows.item(i));
-          }
+      tx.executeSql("SELECT * FROM incident;", [], (_, resultSet) => {
+        const fetchedData = resultSet.rows._array;
+        const labels = fetchedData.map((item) =>
+          item.date.split("-").slice(0, 2).join("-")
+        );
+        const datasetData = fetchedData.map((item) => item.dosage);
 
-          const dataString = JSON.stringify(fetchedData);
-
-          const fileUri = FileSystem.documentDirectory + "incidentData.json";
-
-          FileSystem.writeAsStringAsync(fileUri, dataString)
-            .then(() => {
-              setFileUri(fileUri);
-              alert(`Data saved to ${fileUri}`);
-            })
-            .catch((error) => {
-              alert("Error saving data to file.");
-              console.error(error);
-            });
-        },
-        (_, error) => {
-          console.log("Error fetching data from database.");
-          console.error(error);
-          return false;
-        }
-      );
+        setData({
+          labels: labels,
+          datasets: [{ data: datasetData }],
+        });
+      });
     });
   }, []);
 
-  const shareFile = useCallback(async () => {
-    if (!fileUri) {
-      alert("Please download the data first.");
-      return;
-    }
-
-    if (!(await Sharing.isAvailableAsync())) {
-      alert("Sharing is not available on this platform.");
-      return;
-    }
-
-    Sharing.shareAsync(fileUri);
-  }, [fileUri]);
+  if (!data) {
+    return <Text>Loading...</Text>;
+  }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button title="Download Data To the Device" onPress={downloadData} />
-      <Button title="Share Data" onPress={shareFile} />
+    <View style={styles.container}>
+      <BarChart
+        data={data}
+        width={Dimensions.get("window").width - 16}
+        height={220}
+        yAxisLabel=""
+        yAxisSuffix="mg"
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "0",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, styles.downloadButton]}>
+          <Text style={styles.buttonText}>Download to Disk</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.shareButton]}>
+          <Text style={styles.buttonText}>Share Data</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+// ... [rest of the code remains unchanged]
+
+export default Graph;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  button: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    margin: 3,
+  },
+  downloadButton: {
+    backgroundColor: "magenta",
+  },
+  shareButton: {
+    backgroundColor: "green",
+  },
+  buttonText: {
+    color: "white",
+  },
+});
 
 export default Graph;
